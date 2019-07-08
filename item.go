d3efa2b67c47da718969c7b69280f91c211830da
seunghyek.cho <shhj1998@postech.ac.kr>
2019-07-08 19:02:48 +0900

    add basic item api

--- item.go
+++ item.go
@@ -1,10 +1,28 @@
 package main
 
 import (
+	"fmt"
 	"github.com/gin-gonic/gin"
+	"github.com/mmcdole/gofeed/rss"
 	"net/http"
 )
 
 func (env *Env) getItem(ctx *gin.Context) {
-	ctx.String(http.StatusOK, "item!")
+	var Id int
+	var RSSLink string
+	var item rss.Item
+
+	rows, err := env.db.Query(`SELECT * FROM Item`)
+
+	if err != nil {
+		ctx.String(http.StatusBadRequest, err.Error())
+	} else {
+		for rows.Next() {
+			rows.Scan(&item.GUID.Value, &item.Title, &item.Link, &item.Enclosure.URL, &item.Description, &item.PubDate)
+
+			fmt.Println(Id, RSSLink)
+		}
+
+		ctx.String(http.StatusOK, "item exists")
+	}
 }
