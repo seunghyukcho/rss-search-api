321637408885c4534096da6c0d76dd5d7a2e42f4
shhj1998 <choseunghyek@gmail.com>
2019-07-11 17:47:52 +0900

    v1.0.1 production (#7)
    * Create rsserver package
    
    * Fixed /channel/items bug that showed duplicates
    
    * Implemented services and create new model for Channel
    
    * Add sample test to Item services
    
    * - Complete unit test of ItemController and service
    
    * - Divide the package rsserver to Channel and Item
    
    * - Add enclosures to Item

--- db.go
+++
@@ -1,9 +0,0 @@
-package main
-
-import (
-	"database/sql"
-)
-
-type Env struct {
-	db *sql.DB
-}
---
+++ rsserver/item/controller_test.go
@@ -0,0 +1,44 @@
+package item_test
+
+import (
+	"encoding/json"
+	"github.com/DATA-DOG/go-sqlmock"
+	"github.com/gin-gonic/gin"
+	"github.com/mmcdole/gofeed"
+	"github.com/shhj1998/rss-search-api/rsserver"
+	"github.com/shhj1998/rss-search-api/rsserver/item"
+	"github.com/stretchr/testify/assert"
+	"io/ioutil"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+)
+
+func TestController_GetItems(t *testing.T) {
+	var answerItems []gofeed.Item
+	answerItems = append(answerItems, gofeed.Item{GUID: "media:news:20190708154212517", Title: "I think it is a good day", Link: "link01", Description: "so do I!", Published: "2019-07-08 06:42:12"})
+	answerItems = append(answerItems, gofeed.Item{GUID: "media:news:20190708154523636", Title: "It's raining outside...", Link: "link02", Description: "I'm getting tired...", Published: "2019-07-09 07:42:12"})
+
+	conn, mock, _ := sqlmock.New()
+	defer conn.Close()
+
+	mockRows := sqlmock.NewRows([]string{"guid", "title", "link", "description", "published", "creator", "url", "length", "type"}).
+		AddRow("media:news:20190708154212517", "I think it is a good day", "link01", "so do I!", "2019-07-08 06:42:12", nil, nil, nil, nil).
+		AddRow("media:news:20190708154523636", "It's raining outside...", "link02", "I'm getting tired...", "2019-07-09 07:42:12", nil, nil, nil, nil)
+
+	mock.ExpectQuery("SELECT").WillReturnRows(mockRows)
+	db := rsserver.DB{Connection: conn}
+	controller := item.Controller{Table: &db}
+
+	gin.SetMode(gin.TestMode)
+
+	w := httptest.NewRecorder()
+	ctx, _ := gin.CreateTestContext(w)
+	controller.GetItems(ctx)
+
+	assert.Equal(t, w.Code, http.StatusOK)
+	res, _ := ioutil.ReadAll(w.Body)
+	ans, _ := json.Marshal(answerItems)
+
+	assert.Equal(t, ans, res)
+}
