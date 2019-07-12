42e0c37ad81e776e918fdaf9b95e762e07b9f946
seunghyek.cho <shhj1998@postech.ac.kr>
2019-07-12 14:48:20 +0900

    Add goroutine that updates db periodically

--- main.go
+++ main.go
@@ -4,14 +4,20 @@ import (
 	"fmt"
 	"github.com/gin-gonic/gin"
 	_ "github.com/go-sql-driver/mysql"
+	"github.com/google/logger"
 	"github.com/joho/godotenv"
 	"github.com/shhj1998/rss-search-api/rsserver"
 	"github.com/shhj1998/rss-search-api/rsserver/channel"
 	"github.com/shhj1998/rss-search-api/rsserver/item"
+	"io/ioutil"
+	"log"
 	"os"
+	"time"
 )
 
 func main() {
+	logger.SetFlags(log.LstdFlags)
+	customLogger := logger.Init("LogUpdate", true, false, ioutil.Discard)
 	if err := godotenv.Load(); err != nil {
 		fmt.Println(err.Error())
 	}
@@ -22,11 +28,22 @@ func main() {
 		panic(err)
 	}
 
-	if err := rssDB.Update(); err != nil {
-		panic(err)
-	}
-
 	defer rssDB.Close()
+
+	go func(db *rsserver.DB) {
+		ticker := time.NewTicker(1 * time.Hour)
+
+		for {
+			select {
+			case <-ticker.C:
+				if err := db.Update(); err != nil {
+					customLogger.Error(err.Error())
+				} else {
+					customLogger.Info("Update RSS Database successfully!")
+				}
+			}
+		}
+	}(&rssDB)
 
 	itemInstance := item.Controller{Table: &rssDB}
 	channelInstance := channel.Controller{Table: &rssDB}
